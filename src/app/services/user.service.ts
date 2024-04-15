2eb2ceaf2246312bdfe97950fda5af9c10558566
BoredDany <martinezq.d@javeriana.edu.co>
2024-04-15 13:52:25 -0500

    example getUser with axio

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/User';
 
+import axios from 'axios';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
   constructor() { }
+
+  getUsersExternal(): Promise < User[] > {
+    return axios.get('https://gruposjaveriana.dynaco.co/grupo23/controllers/user/').then(response => response.data);
+  }
+
+  getUsersInternal(): Promise < User[] > {
+    return axios.get('http://localhost:8080/grupo23/controllers/user/').then(response => response.data);
+  }
 }
---
+++ src/models/User.ts
@@ -0,0 +1,13 @@
+export class User {
+
+    constructor(
+        public idUser: number,
+        public name: string,
+        public email: string,
+        public phone: number,
+        public photo: string,
+        public status: string
+
+    ){}
+
+}
