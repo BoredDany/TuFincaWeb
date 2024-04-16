89e69587669d88ba7d61e117b68da5560c08c70f
BoredDany <martinezq.d@javeriana.edu.co>
2024-04-16 15:37:01 -0500

    userservice

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,11 +9,89 @@ export class UserService {
 
   constructor() { }
 
-  getUsersExternal(): Promise < User[] > {
-    return axios.get('https://gruposjaveriana.dynaco.co/grupo23/controllers/user/').then(response => response.data);
+  //get users 
+  getUsersServer(): Promise < User[] > {
+    return axios.get(`https://gruposjaveriana.dynaco.co/grupo23/controllers/user/`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Error getUsersServer!', error);
+    });;
   }
 
-  getUsersInternal(): Promise < User[] > {
-    return axios.get('http://localhost:8080/grupo23/controllers/user/').then(response => response.data);
+  getUsersLocal(): Promise < User[] > {
+    return axios.get(`http://localhost:8080/grupo23/controllers/user/`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Error getUsersLocal!', error);
+    });;
   }
+
+  //get user by id
+  getUserByIdServer(id: number): Promise < User > {
+    return axios.get(`https://gruposjaveriana.dynaco.co/grupo23/controllers/user/${id}`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Error getUserByIdServer!', error);
+    });;;
+  }
+
+  getUserByIdLocal(id: number): Promise < User > {
+    return axios.get(`http://localhost:8080/grupo23/controllers/user/${id}`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Error getUserByIdLocal!', error);
+    });;;
+  }
+
+  //post user
+  postUserServer(user: User): Promise < User[] > {
+    return axios.post(`https://gruposjaveriana.dynaco.co/grupo23/controllers/user/`, user)
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Error postUserServer!', error);
+    });;;
+  }
+
+  postUserLocal(user: User): Promise < User[] > {
+    return axios.post(`http://localhost:8080/grupo23/controllers/user/`, user)
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Error postUserLocal!', error);
+    });;;
+  }
+
+  //put user
+  putUserServer(id: number, user: User): Promise<User> {
+    return axios.put(`https://gruposjaveriana.dynaco.co/grupo23/controllers/user/${id}`, user)
+      .then(response => response.data)
+      .catch(error => {
+        console.error('Error putUserServer!', error);
+      });;;
+  }
+
+  putUserLocal(id: number, user: User): Promise<User> {
+    return axios.put(`http://localhost:8080/grupo23/controllers/user/${id}`, user)
+      .then(response => response.data)
+      .catch(error => {
+        console.error('Error putUserLocal!', error);
+      });;;
+  }
+
+  //delete user
+  deleteUserServer(id: number): Promise<void> {
+    return axios.delete(`https://gruposjaveriana.dynaco.co/grupo23/controllers/user/${id}`)
+      .then(response => response.data)
+      .catch(error => {
+        console.error('Error deleteUserServer!', error);
+      });;;
+  }
+
+  deleteUserLocal(id: number): Promise<void> {
+    return axios.delete(`http://localhost:8080/grupo23/controllers/user/${id}`)
+      .then(response => response.data)
+      .catch(error => {
+        console.error('Error deleteUserLocal!', error);
+      });;;
+  }
+
 }
