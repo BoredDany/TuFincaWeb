ec949f0fa1c82f5ec5572097b0470cdb2f47c3f7
Matias Gonzalez <103151423+M4TI4SGV@users.noreply.github.com>
2024-04-16 17:26:55 -0500

    FORMULARIO MAIN
    FORMULARIO MAIN

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,23 @@
+// main.component.ts
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrls: ['./main.component.css'] // Cambiado de 'styleUrl' a 'styleUrls'
 })
 export class MainComponent {
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required, Validators.pattern(/^\d+$/)]),
+    photo: new FormControl(''),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
