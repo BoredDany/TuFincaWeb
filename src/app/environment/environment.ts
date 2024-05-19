87a55b9bb6a0d0f810342e07f24338139f4aad20
TalkySafe143 <sebastian.galindo143@gmail.com>
2024-05-19 10:10:47 -0500

    Testing gitignore

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,8 +1,6 @@
-import * as dotenv from 'dotenv'
-dotenv.config();
 
 export const environment = {
   production: false,
   backendURL: "http://localhost:8080/grupo23",
-  GEOCODER_API_KEY: process.env['GEOCODER_API_KEY'] || ""
+  GEOCODER_API_KEY: "Jum"
 }
